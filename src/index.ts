12001f281f054a8b4861ef6324b64938a4944280
Michael Ahern <michael.ahern@gmail.com>
2026-03-07 05:33:12 +0000

    Multiple Tools

--- src/index.ts
+++ src/index.ts
@@ -8,24 +8,33 @@ export class MyMCP extends McpAgent {
     });
 
     async init() {
-        this.server.registerTool('library', {
-            title: 'CellarTracker Library',
-            description: 'Fetches your entire wine library data from CellarTracker'
+        this.ct_tool('wine', 'CellarTracker Wine List', 'Get your current wine library list from CellarTracker.', 'List');
+        this.ct_tool('wine_bottles', 'CellarTracker Wine Bottles', 'Get your current wine bottle inventory from CellarTracker.', 'Inventory');
+        this.ct_tool('wine_consumed', 'CellarTracker Wine Consumed', 'Get your consumed wine bottles from CellarTracker.', 'Consumed');
+        this.ct_tool('wine_pending', 'CellarTracker Wine Pending', 'Get your wine inventory pending delivery from CellarTracker.', 'Pending');
+        this.ct_tool('wine_purchases', 'CellarTracker Wine Purchases', 'Get your wine purchase history from CellarTracker.', 'Purchase');
+    }
+
+    async ct_tool(name: string, title: string, description: string, table: 'List' | 'Inventory' | 'Purchase' | 'Pending' | 'Consumed' | 'Availability') {
+        this.server.registerTool(name, {
+            title: title,
+            description: description
         },
         async () => {
             const username = await this.env.CELLARTRACKER_USERNAME.get();
             const password = await this.env.CELLARTRACKER_PASSWORD.get();
-            const url = `https://www.cellartracker.com/xlquery.asp?User=${encodeURIComponent(username)}&Password=${encodeURIComponent(password)}&Format=csv`;
+            const url = `https://www.cellartracker.com/xlquery.asp?User=${encodeURIComponent(username)}&Password=${encodeURIComponent(password)}&Format=csv&Table=${encodeURIComponent(table)}`;
             const response = await fetch(url);
+
             if (!response.ok) {
                 return {
                     content: [{ type: 'text', text: `Failed to fetch data from CellarTracker: ${response.status} ${response.statusText}` }],
                     isError: true
                 };
             }
-            const text = await response.text();
+
             return {
-                content: [{ type: 'text', text }]
+                content: [{ type: 'text', text: await response.text() }]
             };
         });
     }
